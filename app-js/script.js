8319962979e26f74752b756585869faf65cb2a71
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-02-22 14:34:19 +0000

    pokedex app v1.3
    If applied, commit will:
    index.html:
    - Rename title to "Pokedex"
    - Add h1
    style.css:
    - Add body style
    - Add dive style
    - Add h1 style
    script.js:
    - changed Charmander height to 1.6
    - added for loop with height conditional and returns name + height + conditional

--- app-js/script.js
+++ app-js/script.js
@@ -6,7 +6,7 @@ let pokemonList = [
     },
     {
         name: "Charmander",
-        height: 0.6,
+        height: 1.6,
         type: ['Fire']
     },
     {
@@ -15,3 +15,16 @@ let pokemonList = [
         type: ['Water']
     }
 ];
+//creates a for loop that iterates over each item in pokemonList and returns the name and height
+
+/*for (i = 0; pokemonList[i]; i++); {
+    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ");
+} */
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ");
+    if (heightConditional = pokemonList[i].height >= 1.0 ? "- Wow that's big " : ""); {
+        document.write(heightConditional);
+    }
+}
