2e830471475df737dfd59c6a125780717aa55ec2
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-02-26 16:49:26 +0000

    script.js for each
    If applied, commit will remove unnecessary code and replace forLoop with forEachLoop

--- app-js/script.js
+++ app-js/script.js
@@ -15,16 +15,10 @@ let pokemonList = [
         type: ['Water']
     }
 ];
-//creates a for loop that iterates over each item in pokemonList and returns the name and height
 
-/*for (i = 0; pokemonList[i]; i++); {
-    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ");
-} */
-
-
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ");
-    if (heightConditional = pokemonList[i].height >= 1.0 ? "- Wow that's big " : ""); {
+pokemonList.forEach((pokemonList) => {
+    document.write(pokemonList.name + " (Height: " + pokemonList.height + ") ");
+    if (heightConditional = pokemonList.height >= 1.0 ? "- Wow that's big " : ""); {
         document.write(heightConditional);
     }
-}
+});
