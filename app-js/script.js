a4ccf330916ba99518b3781e80489fe69dec8ede
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-02-27 18:44:55 +0000

    Update script.js
    If applied, commit will:
    - wrap pokemonList array in IIFE
    - added 3 public functions: add, getAll and find
    - adjusted forEach loop so that it still works after IIFE change

--- app-js/script.js
+++ app-js/script.js
@@ -1,22 +1,44 @@
-let pokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 0.7,
-        type: ['Grass', 'Poison']
-    },
-    {
-        name: "Charmander",
-        height: 1.6,
-        type: ['Fire']
-    },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        type: ['Water']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            type: ['Grass', 'Poison']
+        },
+        {
+            name: "Charmander",
+            height: 1.6,
+            type: ['Fire']
+        },
+        {
+            name: "Squirtle",
+            height: 0.5,
+            type: ['Water']
+        }
+    ];
 
-pokemonList.forEach((pokemonList) => {
+    function add(pokemon) {
+        if (pokemon.typeof === Object && Object.keys(pokemon) === Object.keys(pokemonList) ? pokemonList.push(pokemon) : console.log("Wrong Data Type") );
+    };
+
+    function getAll() {
+        return pokemonList;
+    };
+
+    function find(pokemon) {
+        pokemonList.name.filter(pokemonList.name === pokemon.name); {
+            return pokemon.name;
+        };
+    };
+
+    return {
+        add: add,
+        getAll: getAll,
+        find: find
+    };
+})();
+
+pokemonRepository.getAll().forEach((pokemonList) => {
     document.write(pokemonList.name + " (Height: " + pokemonList.height + ") ");
     if (heightConditional = pokemonList.height >= 1.0 ? "- Wow that's big " : ""); {
         document.write(heightConditional);
