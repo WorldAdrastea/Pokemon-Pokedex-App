dae65e1fe2885f8194d2a92f1cc96968772e4a35
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-03-06 16:17:40 +0000

    DOM Interaction p3
    If applied, commit will:
    - add comments throughout
    - added button styling for hover and focus to .css
    - added button event listener in .js
    - added showDetails function in .js

--- app-js/script.js
+++ app-js/script.js
@@ -1,3 +1,4 @@
+//IIFE with pokemon list
 let pokemonRepository = (function () {
     let pokemonList = [
         {
@@ -16,19 +17,19 @@ let pokemonRepository = (function () {
             type: ['Water']
         }
     ];
-
+    //function to add pokemon to array list
     function add(pokemon) {
         if (typeof(pokemon) === "object" && Object.keys(pokemonList[0]).every((val, i, p) => val === p[i]) ? pokemonList.push(pokemon) : console.log("Wrong Data Type"));
     };
-
+    //function to get all of pokemonList
     function getAll() {
         return pokemonList;
     };
-
+    //function that finds pokemon (WIP)
     function find(pokemonName) {
        return pokemonList.filter(pokemon => pokemon.name === pokemonName);
     };
-
+    //function that creates a list of pokemon from array + event listener to show details
     function addListItem(pokemon) {
         let pokemonUl = document.querySelector('ul');
         let listItem = document.createElement('li');
@@ -38,16 +39,22 @@ let pokemonRepository = (function () {
         button.classList.add('pokemonButtonStyle');
         listItem.appendChild(button);
         pokemonUl.appendChild(listItem);
-    }
-
+        button.addEventListener('click', showDetails(pokemon));
+    };
+    //function to show the details of pokemon (console log for now)
+    function showDetails(pokemon) {
+        console.log(pokemon.name);
+    };
+    //returns functions so that they can be used outside of IIFE
     return {
         add: add,
         getAll: getAll,
         find: find,
         addListItem: addListItem,
+        showDetails: showDetails,
     };
 })();
-
+//runs addListItem for each of the pokemon in pokemonList
 pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
