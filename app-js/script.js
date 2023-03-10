7acec3026eaccc328fe9f05127ef15bb86b9c1ca
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-03-10 15:29:00 +0000

    Pokedex v4 1.7
    If applied, commit will:
    - Add polyfill and fetch scripts into index.html
    - Added functions inside repository to load data from external PokeAPI
    - Emptied pokemonList array to allow to load Pokemon data in
    - Fixes the add Pokemon function
    - Edited showDetails() function to load the Pokemon details from API

--- app-js/script.js
+++ app-js/script.js
@@ -1,25 +1,11 @@
 //IIFE with pokemon list
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: "Bulbasaur",
-            height: 0.7,
-            type: ['Grass', 'Poison']
-        },
-        {
-            name: "Charmander",
-            height: 1.6,
-            type: ['Fire']
-        },
-        {
-            name: "Squirtle",
-            height: 0.5,
-            type: ['Water']
-        }
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+
     //function to add pokemon to array list
     function add(pokemon) {
-        if (typeof(pokemon) === "object" && Object.keys(pokemonList[0]).every((val, i, p) => val === p[i]) ? pokemonList.push(pokemon) : console.log("Wrong Data Type"));
+        if (typeof(pokemon) === "object" && Object.keys(pokemonList).every((val, i, p) => val === p[i]) ? pokemonList.push(pokemon) : console.log("Wrong Data Type"));
     };
     //function to get all of pokemonList
     function getAll() {
@@ -45,8 +31,43 @@ let pokemonRepository = (function () {
     };
     //function to show the details of pokemon (console log for now)
     function showDetails(pokemon) {
-        console.log(pokemon.name);
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon.name);
+        });
     };
+
+    //fetch PokeAPI
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
+        })
+      };
+
+      //Loads details of pokemon
+      function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+      };
+
     //returns functions so that they can be used outside of IIFE
     return {
         add: add,
@@ -54,9 +75,14 @@ let pokemonRepository = (function () {
         find: find,
         addListItem: addListItem,
         showDetails: showDetails,
+        loadList: loadList,
+        loadDetails: loadDetails,
     };
 })();
-//runs addListItem for each of the pokemon in pokemonList
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-});
+
+//Using loadList to load data for pokemon + runs addListItem for each of the pokemon in pokemonList
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon);
+    });
+  });
